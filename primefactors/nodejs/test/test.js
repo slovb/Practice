5db260add24d8ccea707de7f3d4679de09153cde
Rickard Nyman <nyman.rickard@gmail.com>
2016-02-10 19:16:10 +0100

    3 => [3]

--- primefactors/nodejs/test/test.js
+++ primefactors/nodejs/test/test.js
@@ -1,12 +1,15 @@
 const assert = require('assert');
 const primefactors = require('../primefactors.js').primefactors;
 
+function primetester(argument, expected) {
+  it('should return [' + expected.toString() + '] when given ' + argument, function() {
+    assert.deepEqual(expected, primefactors(argument));
+  });
+};
+
 describe('primefactors', function() {
-  it('should return [] when given 1', function() {
-    assert.deepEqual([], primefactors(1));
-  });
-  it('should return [2] when given 2', function() {
-    assert.deepEqual([2], primefactors(2));
-  });
+  primetester(1, []);
+  primetester(2, [2]);
+  primetester(3, [3]);
 });
 
