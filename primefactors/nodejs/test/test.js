a0eb458c7a3da490d60d6e17de9bd72f8c30d665
Rickard Nyman <nyman.rickard@gmail.com>
2016-02-10 19:09:41 +0100

    Test 2 => [2]

--- primefactors/nodejs/test/test.js
+++ primefactors/nodejs/test/test.js
@@ -5,5 +5,8 @@ describe('primefactors', function() {
   it('should return [] when given 1', function() {
     assert.deepEqual([], primefactors(1));
   });
+  it('should return [2] when given 2', function() {
+    assert.deepEqual([2], primefactors(2));
+  });
 });
 
