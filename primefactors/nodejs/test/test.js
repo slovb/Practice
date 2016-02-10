3f239c776519896baddfc070a2b473b6b4eb583f
Rickard Nyman <nyman.rickard@gmail.com>
2016-02-10 19:08:28 +0100

    deepEqual <3

--- primefactors/nodejs/test/test.js
+++ primefactors/nodejs/test/test.js
@@ -3,7 +3,7 @@ const primefactors = require('../primefactors.js').primefactors;
 
 describe('primefactors', function() {
   it('should return [] when given 1', function() {
-    assert.equal([], primefactors(1));
+    assert.deepEqual([], primefactors(1));
   });
 });
 
