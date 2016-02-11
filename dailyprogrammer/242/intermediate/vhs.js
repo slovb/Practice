2f3be0185a2f7afe1c6bf84bb665fc0f016b1045
Rickard Nyman <nyman.rickard@gmail.com>
2016-02-11 19:44:50 +0100

    Inputdecoding and output format

--- dailyprogrammer/242/intermediate/vhs.js
+++ dailyprogrammer/242/intermediate/vhs.js
@@ -1,4 +1,25 @@
-exports.vhs = function(timetable) {
-  return 3;
+var decode = function(timetable) {
+  var output = [];
+  var length = timetable.length;
+  for (var i = 0; i < length; ++i) {
+    var row = timetable[i].split(' ');
+    output.push({
+      start: parseInt(row[0]),
+      end: parseInt(row[1]),
+      name: row.slice(2).join(' ')
+    });
+  }
+  return output;
+}
+
+var vhs = function(timetable) {
+  var decoded = decode(timetable);
+  return [
+    undefined,
+    undefined,
+    undefined
+  ];
 };
+
+exports.vhs = vhs;
 
