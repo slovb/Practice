a417c02f28f77139f85ff51fedd11ea497feea11
Rickard Nyman <nyman.rickard@gmail.com>
2016-02-13 19:18:57 +0100

    Recursive nodejs solution
    It will smash the stack if the input list is large enough.

--- codekata/2/day1/chop.js
+++ codekata/2/day1/chop.js
@@ -1,4 +1,19 @@
-module.exports = function(needle, haystack) {
-  return haystack.indexOf(needle);
+var chop = function(needle, haystack) {
+  if (haystack.length === 0) {
+    return -1;
+  }
+  var midpoint = Math.floor(haystack.length / 2);
+  if (needle === haystack[midpoint]) {
+    return midpoint;
+  }
+  else if (needle < haystack[midpoint]) {
+    return chop(needle, haystack.slice(0, midpoint));
+  }
+  else { // needle > haystack[midpoint]
+    var q = chop(needle, haystack.slice(midpoint + 1));
+    return q === -1 ? -1 : 1 + midpoint + q;
+  }
 };
+
+module.exports = chop;
 
