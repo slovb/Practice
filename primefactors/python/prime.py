a34cb4998483e6e5f1e5e89d3ae805f355250e24
Rickard Nyman <nyman.rickard@gmail.com>
2016-02-15 22:16:52 +0100

    Unrolled the factors of 2 to skip even numbers

--- primefactors/python/prime.py
+++ primefactors/python/prime.py
@@ -1,13 +1,20 @@
 def factorize(n):
     factors = []
+    # Factors of 2 can be unrolled to allow steps of 2 in the loop
     i = 2
-    while (n > 1):
-        while (n % i == 0):
+    while n % i == 0:
+        factors += [i]
+        n //= i
+    # Primary factorization loop initialized at 3 with steps of 2
+    i = 3
+    while i <= n:
+        while n % i == 0:
             factors += [i]
-            n /= i
-        i += 1
+            n //= i
+        i += 2
     return factors
 
 if __name__ == '__main__':
     import sys
     factors(int(sys.argv[1]))
+
